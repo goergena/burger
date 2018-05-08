228eb0bd6146bdac145f2aec93138c53cb1f28c2
goergena <goergena@users.noreply.github.com>
2018-05-08 11:52:40 -0400

    connection edits

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,35 @@
 var mysql = require("mysql");
 
+var connection;
+
+/*
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "burger_db"
-  });
+  }); */
 
-  if (proccess.env.JAWSDB_URL) {
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    var connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "root",
-      database: "burger_db"
-    });
+if (proccess.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+}
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
   }
-  
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
 
-  //export the connection
+  console.log("connected as id " + connection.threadId);
+});
 
-  module.exports = connection;
+//export the connection
+
+module.exports = connection;
